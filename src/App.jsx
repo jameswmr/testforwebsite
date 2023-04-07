00aaf81ec62cd6ee9a7d3f4d25ca98ff327ba125
jameswmr <jamescherry9236@gmail.com>
2023-04-07 13:44:51 +0800

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import my from './assets/me.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div class="container">
       
         <div class="title">
-          <img src="../me.jpg" />
+          <img src={my} />
             <div class="title-header">
                 <h1>Welcome to My Website</h1>
                 <p>I'm a sophomore major in NTU CSIE.</p> 
